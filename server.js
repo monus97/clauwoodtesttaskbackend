f97fe1afc155b88dc332edd587b5c47a5973b736
monus97 <monus3871@gmail.com>
2024-07-24 10:54:57 +0530

    working on socket connection

---
+++ server.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(cors());
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("message", (message) => {
+    console.log(`Received: ${JSON.stringify(message)}`);
+    io.emit("message", message);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, () => {
+  console.log(`Server is running on port:${PORT}`);
+});
